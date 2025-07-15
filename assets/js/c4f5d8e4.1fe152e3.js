89777aa18e3d0729ece419c26f7fa0a5cc7ecb7f
hyochan <hyochan@users.noreply.github.com>
2025-07-15 20:55:10 +0000

    deploy: e9e9d0110f62d8b2e4d5df5a1c4f6d79b032a479

---
+++ assets/js/c4f5d8e4.1fe152e3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[2634],{192:(e,s,t)=>{t.r(s),t.d(s,{default:()=>u});var c=t(6540);t(6025);function u(){return(0,c.useEffect)((()=>{location.href="/docs/get-started"}),[]),null}}}]);
