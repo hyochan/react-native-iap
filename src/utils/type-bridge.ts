56a5ec6b248d051d6310379c919db72e5505074c
Phecda Su <szp12345141@gmail.com>
2026-02-06 11:28:49 +0800

    fix: add missing 'non-renewing-subscription' in normalization

--- src/utils/type-bridge.ts
+++ src/utils/type-bridge.ts
@@ -93,6 +93,7 @@ function normalizeProductTypeIOS(value?: Nullable<string>): ProductTypeIOS {
       return 'auto-renewable-subscription';
     case 'nonrenewingsubscription':
     case 'non_renewing_subscription':
+    case 'non-renewing-subscription':
       return 'non-renewing-subscription';
     default:
       if (value) {
